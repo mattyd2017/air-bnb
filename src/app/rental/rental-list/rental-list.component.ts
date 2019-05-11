d061f4ce8db59e19f72680bd7ed0962dbc5c59a8
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-05-11 14:06:37 +0100

    list detail added

---
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -0,0 +1,32 @@
+import { Rental } from './../shared/rental.model';
+import { RentalService } from './../shared/rental.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-rental-list',
+  templateUrl: './rental-list.component.html',
+  styleUrls: ['./rental-list.component.scss']
+})
+export class RentalListComponent implements OnInit {
+  rentals: Rental[] = [];
+
+  constructor(private rentalService: RentalService) { }
+
+  ngOnInit() {
+
+   const rentalObservable = this.rentalService.getRentals();
+   rentalObservable.subscribe(
+     (rentals: Rental[]) => {
+        this.rentals = rentals;
+
+     },
+     (err) => {
+
+     },
+     () => {
+
+     });
+  }
+
+}
