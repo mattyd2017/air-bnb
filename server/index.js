7c5119423f6a6e2e05f3a795cf0926b6748f37e3
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-05-22 00:06:06 +0100

    node api set up

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const app = express();
-app.get('/rentals', function(req, res) {
-    res.json({'success': true});
+const mongooose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+const Rental = require('./models/rental');
+
+const rentalRoutes = require('./routes/rentals');
+
+mongooose.connect(config.DB_URI).then(() => {
+    const fakeDb = new FakeDb();
+    fakeDb.seedDb();
 });
+
+const app = express();
+
+app.use('/api/v1/rentals', rentalRoutes);
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function(){
