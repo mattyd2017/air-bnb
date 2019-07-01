70bf8afb6429fd21a2810f9fcf9d1eb596628814
mattyd2017 <32998214+mattyd2017@users.noreply.github.com>
2019-07-01 05:31:15 +0100

    ability to create a booking using the datapaicker with validation and error handling and added notification messages for the user and improved the interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RentalModule } from './rental/rental.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
@@ -27,11 +30,14 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    ToastrModule.forRoot(),
     RentalModule,
     RouterModule.forRoot(routes),
     CommonModule,
     AuthModule,
-    FormsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
